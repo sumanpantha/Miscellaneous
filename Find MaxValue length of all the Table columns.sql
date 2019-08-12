a980fc5e2f12c0f2f92c0d829caa65b511c5354f
suman pantha <hrtpantha@gmail.com>
2019-08-12 11:00:54 +0545

    Create Find MaxValue length of all the Table columns.sql

---
+++ Find MaxValue length of all the Table columns.sql
@@ -0,0 +1,7 @@
+--This query give the max lenght of all the columns of all tables in a database which has datatype NVARCHAR(MAX)
+-- So that we can restrict to appropriate length size of the datatype.
+
+SELECT 
+	'select ''['+TABLE_NAME+'.'+COLUMN_NAME  +']'' as [TableAndColName] , Max(len(['+COLUMN_NAME+'])) AS ColMaxLenght'+ ' FROM ['+TABLE_NAME+'] UNION ALL'  
+FROM INFORMATION_SCHEMA.COLUMNS WHERE 
+DATA_TYPE = 'nvarchar' and CHARACTER_MAXIMUM_LENGTH = -1
