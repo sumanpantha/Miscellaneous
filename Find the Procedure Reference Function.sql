4bd31978ea7ad42a3e02c125d714f06e8c0105ef
suman pantha <hrtpantha@gmail.com>
2020-02-05 15:34:17 +0545

    Create Find the Procedure Reference Function.sql

---
+++ Find the Procedure Reference Function.sql
@@ -0,0 +1,7 @@
+SELECT 
+    o.name AS ReferencingObject, 
+    sd.referenced_entity_name AS ReferencedObject
+FROM sys.sql_expression_dependencies  AS sd
+INNER JOIN sys.objects AS o
+    ON o.object_id = sd.referencing_id
+WHERE sd.referenced_entity_name = 'ufn_GetAllTransactionDateWiseOnlyBranch'; -- Procedure Name
