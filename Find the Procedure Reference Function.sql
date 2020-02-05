36464cfa2b8368fab3261a7021f6375c786b7374
suman pantha <hrtpantha@gmail.com>
2020-02-05 15:36:13 +0545

    Update Find the Procedure Reference Function.sql

--- Find the Procedure Reference Function.sql
+++ Find the Procedure Reference Function.sql
@@ -4,4 +4,4 @@ SELECT
 FROM sys.sql_expression_dependencies  AS sd
 INNER JOIN sys.objects AS o
     ON o.object_id = sd.referencing_id
-WHERE sd.referenced_entity_name = 'ufn_GetAllTransactionDateWiseOnlyBranch'; -- Procedure Name
+WHERE sd.referenced_entity_name = 'ufn_GetAllTransactionDateWiseOnlyBranch'; -- Function Name
