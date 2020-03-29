617cf2dd982a0064b55ee777cc062a3d82159567
suman pantha <hrtpantha@gmail.com>
2020-03-29 14:57:16 +0545

    Update Child and Parent Table List.sql

--- Child and Parent Table List.sql
+++ Child and Parent Table List.sql
@@ -1,6 +1,6 @@
 select 
-    object_name(parent_object_id), 
-    object_name(referenced_object_id)
+    object_name(parent_object_id) [ChildTable], 
+    object_name(referenced_object_id) [ParentTable]
 from 
     sys.foreign_keys
 WHERE 
