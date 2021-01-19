5b4814877ff7aa22a92d7e2e7fedee80bc26cb70
suman pantha <hrtpantha@gmail.com>
2021-01-19 13:12:59 +0545

    Update Find MaxValue length of all the Table columns.sql

--- Find MaxValue length of all the Table columns.sql
+++ Find MaxValue length of all the Table columns.sql
@@ -2,7 +2,7 @@
 -- So that we can restrict to appropriate length size of the datatype.
 
 SELECT 
-	'select ''['+TABLE_NAME+'.'+COLUMN_NAME  +']'' as [TableAndColName] , Max(len(['+COLUMN_NAME+'])) AS ColMaxLenght'+ ' FROM ['+TABLE_NAME+'] UNION ALL'  
+	'select ''['+TABLE_NAME+'.'+COLUMN_NAME  +']'' as [TableAndColName] , Max(len(['+COLUMN_NAME+'])) AS ColMaxDataLength'+ ' FROM ['+TABLE_NAME+'] UNION ALL'  
 FROM INFORMATION_SCHEMA.COLUMNS WHERE 
 DATA_TYPE = 'nvarchar' and CHARACTER_MAXIMUM_LENGTH = -1
 ORDER BY TABLE_NAME
